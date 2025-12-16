f20a083ce246ad6a426f26a116b3d63eafbf77ce
Akshay Dhayal <akshaydhayalkd99@gmail.com>
2025-12-16 20:27:37 +0530

    Update commandsData.js

--- services/commandsData.js
+++ services/commandsData.js
@@ -1,15 +1,15 @@
 // services/commandsData.js
 const commands = [
-  "dig @3.82.20.22 -p 5353 epoch +short               - Show Solana current epoch progress",
-  "dig @3.82.20.22 -p 5353 tps +short                 - Show Solana Transactions per second(TPS) records",
-  "dig @3.82.20.22 -p 5353 blocktime +short           - Show Solana blocktime metrics records",
-  "dig @3.82.20.22 -p 5353 top-validators +short      - Show top 10 Solana validators",
-  "dig @3.82.20.22 -p 5353 price-chart +short         - Show Solana price chart and metrics",
-  "dig @3.82.20.22 -p 5353 sol-supply +short          - Show Total Solana Supply and Stake Data",
-  "dig @3.82.20.22 -p 5353 stake-data +short          - Get Solana staking statistics",  
-  "dig @3.82.20.22 -p 5353 stake-graph +short         - Get Solana Stake Growth Over Time",  
-  "dig @3.82.20.22 -p 5353 stake-average-size +short  - Get Average SOL Staked size data",  
-  "dig @3.82.20.22 -p 5353 help +short                - Show all commands list"
+  "dig @100.31.3.72 -p 5353 epoch +short               - Show Solana current epoch progress",
+  "dig @100.31.3.72 -p 5353 tps +short                 - Show Solana Transactions per second(TPS) records",
+  "dig @100.31.3.72 -p 5353 blocktime +short           - Show Solana blocktime metrics records",
+  "dig @100.31.3.72 -p 5353 top-validators +short      - Show top 10 Solana validators",
+  "dig @100.31.3.72 -p 5353 price-chart +short         - Show Solana price chart and metrics",
+  "dig @100.31.3.72 -p 5353 sol-supply +short          - Show Total Solana Supply and Stake Data",
+  "dig @100.31.3.72 -p 5353 stake-data +short          - Get Solana staking statistics",  
+  "dig @100.31.3.72 -p 5353 stake-graph +short         - Get Solana Stake Growth Over Time",  
+  "dig @100.31.3.72 -p 5353 stake-average-size +short  - Get Average SOL Staked size data",  
+  "dig @100.31.3.72 -p 5353 help +short                - Show all commands list"
 ];
 
 function getCommandsList() {
